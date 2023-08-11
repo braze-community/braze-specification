f1d05a673ad111d9fc76b67c28f0cbc44ec13e0e
Mark <mark@remarkablemark.org>
2023-08-10 23:28:17 -0400

    refactor(postman): refactor migrations

--- postman/migrations/2023-08-09-create-multiple-catalog-items-add-missing-schema-in-body-parameter.ts
+++ postman/migrations/2023-08-09-create-multiple-catalog-items-add-missing-schema-in-body-parameter.ts
@@ -1,38 +1,27 @@
-// @ts-nocheck
 import collection from '../collection.json';
-import { writeCollection } from '../../utils';
+import { traverse, writeCollection } from '../../utils';
 
-const catalogs = collection.item.find(({ name }) => name === 'Catalogs');
+traverse(collection, '', null, (value, key, parent) => {
+  if (key === 'name' && value === 'Create Multiple Catalog Items') {
+    const body = {
+      items: [
+        {
+          id: 'restaurant1',
+          Name: 'Restaurant1',
+          City: 'New York',
+          Cuisine: 'American',
+          Rating: 5,
+          Loyalty_Program: true,
+          Created_At: '2022-11-01T09:03:19.967+00:00',
+        },
+      ],
+    };
 
-const catalogManagement = catalogs.item.find(
-  ({ name }: { name: string }) => name === 'Catalog Items',
-);
-
-const asynchronous = catalogManagement.item.find(
-  ({ name }) => name === 'Asynchronous',
-);
-
-const createMultipleCatalogItems = asynchronous.item.find(
-  ({ name }) => name === 'Create Multiple Catalog Items',
-);
-
-const body = {
-  items: [
-    {
-      id: 'restaurant1',
-      Name: 'Restaurant1',
-      City: 'New York',
-      Cuisine: 'American',
-      Rating: 5,
-      Loyalty_Program: true,
-      Created_At: '2022-11-01T09:03:19.967+00:00',
-    },
-  ],
-};
-
-createMultipleCatalogItems.request.body = {
-  mode: 'raw',
-  raw: JSON.stringify(body, null, 2),
-};
+    parent.request.body = {
+      mode: 'raw',
+      raw: JSON.stringify(body, null, 2),
+    };
+  }
+});
 
 writeCollection(collection);

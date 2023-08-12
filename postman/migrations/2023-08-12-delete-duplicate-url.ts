29c9d9d3d4b741d79396437e299d784bf1bd4ccb
Mark <mark@remarkablemark.org>
2023-08-12 02:11:49 -0400

    fix(postman): add migration to delete duplicate url

---
+++ postman/migrations/2023-08-12-delete-duplicate-url.ts
@@ -0,0 +1,25 @@
+import collection from '../collection.json';
+import { traverse, writeCollection } from '../../utils';
+
+traverse(collection, '', null, (value, key, parent) => {
+  if (
+    key === 'name' &&
+    value === "Update User's Subscription Group Status V2" &&
+    parent?.request?.url?.raw ===
+      'https://{{instance_url}} /v2/subscription/status/set'
+  ) {
+    Object.keys(parent).forEach((key) => {
+      delete parent[key];
+    });
+  }
+});
+
+traverse(collection, '', null, (value, key, parent) => {
+  if (key === 'name' && value === 'Email') {
+    parent.item = parent.item.filter(
+      (item: object) => Object.keys(item).length,
+    );
+  }
+});
+
+writeCollection(collection);

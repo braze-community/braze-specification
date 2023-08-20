4b1ebcd2710c3ba776860d7c2ff6d0cc41265217
Mark <mark@remarkablemark.org>
2023-08-20 17:38:02 -0400

    refactor(openapi): consolidate migrations

--- openapi/migrations/2023-08-10-fix-servers-and-relative-urls.ts
+++ openapi/migrations/2023-08-10-fix-servers-and-relative-urls.ts
@@ -1,5 +1,8 @@
 import spec from '../spec.json';
 import { traverse, writeSpec } from '../../utils';
+import options from '../../postman-to-openapi.json';
+
+const url = options.additionalVars.instance_url.replace('https:', '');
 
 traverse(spec, '', null, (value, key, parent) => {
   if (key === 'servers') {
@@ -48,6 +51,11 @@ traverse(spec, '', null, (value, key, parent) => {
         description: 'REST endpoint for instance EU-02',
       },
     ];
+  }
+
+  if (typeof key === 'string' && key.startsWith(url)) {
+    parent[key.replace(url, '')] = value;
+    delete parent[key];
   }
 });
 

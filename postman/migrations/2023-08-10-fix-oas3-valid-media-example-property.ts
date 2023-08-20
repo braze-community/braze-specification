e80ee75c6eb4709cad7e0fab781ea63ce387813b
Mark <mark@remarkablemark.org>
2023-08-20 16:51:55 -0400

    refactor(postman): consolidate migrations

--- postman/migrations/2023-08-10-fix-oas3-valid-media-example-property.ts
+++ postman/migrations/2023-08-10-fix-oas3-valid-media-example-property.ts
@@ -1,7 +1,13 @@
 import collection from '../collection.json';
 import { traverse, writeCollection } from '../../utils';
 
+const NUMERIC_REGEX = /^\d+$/;
+
 traverse(collection, '', null, (value, key, parent) => {
+  if (key === 'name' && value === 'Create Scheduled Messages') {
+    delete parent.request.body.options;
+  }
+
   if (key === 'value') {
     switch (value) {
       case 'true':
@@ -9,6 +15,11 @@ traverse(collection, '', null, (value, key, parent) => {
         break;
       case 'false':
         parent[key] = false;
+        break;
+      default:
+        if (NUMERIC_REGEX.test(value)) {
+          parent[key] = parseInt(value, 10);
+        }
         break;
     }
   }

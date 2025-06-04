b5e74aa04c5483ff72e903818645e419adb7372c
Mark <mark@remarkablemark.org>
2025-06-04 12:11:45 -0400

    fix(postman): fix migrations and regenerate openapi spec

--- postman/migrations/2023-08-09-fix-catalog-json-schema-body.ts
+++ postman/migrations/2023-08-09-fix-catalog-json-schema-body.ts
@@ -2,55 +2,47 @@ import collection from '../collection.json';
 import { traverse, writeCollection } from '../../utils';
 
 traverse(collection, '', null, (value, key, parent) => {
-  if (key !== 'name') {
+  if (key !== 'name' || typeof parent.request?.method !== 'string') {
     return;
   }
 
   switch (value) {
-    case 'Create Catalog':
+    case 'Create catalog':
       return (parent.request.body = {
         mode: 'raw',
-        raw: JSON.stringify(
-          {
-            catalogs: [
-              {
-                name: 'restaurants',
-                description: 'My Restaurants',
-                fields: [{ name: 'id', type: 'string' }],
-              },
-            ],
-          },
-          null,
-          2,
-        ),
+        raw: JSON.stringify({
+          catalogs: [
+            {
+              name: 'restaurants',
+              description: 'My Restaurants',
+              fields: [{ name: 'id', type: 'string' }],
+            },
+          ],
+        }),
       });
 
-    case 'Create Multiple Catalog Items':
+    case 'Create multiple catalog items':
       return (parent.request.body = {
         mode: 'raw',
-        raw: JSON.stringify(
-          {
-            items: [
-              {
-                id: 'restaurant1',
-                Name: 'Restaurant1',
-                City: 'New York',
-                Cuisine: 'American',
-                Rating: 5,
-                Loyalty_Program: true,
-                Created_At: '2022-11-01T09:03:19.967+00:00',
-              },
-            ],
-          },
-          null,
-          2,
-        ),
+        raw: JSON.stringify({
+          items: [
+            {
+              id: 'restaurant1',
+              Name: 'Restaurant1',
+              City: 'New York',
+              Cuisine: 'American',
+              Rating: 5,
+              Loyalty_Program: true,
+              Created_At: '2022-11-01T09:03:19.967+00:00',
+            },
+          ],
+        }),
       });
 
-    case 'Edit Multiple Catalog Items':
+    case 'Edit multiple catalog items':
       return (parent.request.body = {
         mode: 'raw',
-        raw: JSON.stringify({ items: [{ id: 'restaurant1' }] }, null, 2),
+        raw: JSON.stringify({ items: [{ id: 'restaurant1' }] }),
       });
   }
 });
